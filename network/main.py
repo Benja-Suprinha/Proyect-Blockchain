78bf6d217403d515fe4cb7cb3c0a0fa630212524
benja <benjamin.ojeda@mail.udp.cl>
2023-11-12 15:56:28 -0300

    Create a simple network p2p

---
+++ network/main.py
@@ -0,0 +1,91 @@
+import argparse
+
+import multiaddr
+import trio
+import libp2p
+from libp2p.peer.peerinfo import info_from_p2p_addr
+from libp2p.typing import TProtocol
+from libp2p.network.stream.net_stream_interface import INetStream
+from libp2p.crypto.secp256k1 import create_new_key_pair
+
+PROTOCOL_ID = TProtocol("/test/1.0.0")
+
+async def _echo_stream_handler(stream: INetStream) -> None:
+    # Wait until EOF
+    req = await stream.read()
+    res = 'Si estas conectado!'
+    print('Se conecto alguien')
+    print('Aca hay que pasarle los bloques que ya existen!')
+    print(req.decode())
+    await stream.write(res.encode())
+    await stream.close()
+
+async def run(port: int, destination:str):
+    localhost_ip = '127.0.0.1'
+    listen_addr = multiaddr.Multiaddr(f"/ip4/127.0.0.1/tcp/{port}")
+
+    import secrets
+    secret = secrets.token_bytes(32)
+
+    host = libp2p.new_host(key_pair = create_new_key_pair(secret))
+    async with host.run(listen_addrs = [listen_addr]):
+
+        print(f'Yo soy {host.get_id().to_string()}')
+
+        if not destination:
+            host.set_stream_handler(PROTOCOL_ID, _echo_stream_handler)
+            print('Esperando conexiones ... ')
+            await trio.sleep_forever()
+        else:
+            maddr = multiaddr.Multiaddr(destination)
+            info = info_from_p2p_addr(maddr)
+            print(info.peer_id.to_string())
+            await host.connect(info)
+
+            stream = await host.new_stream(info.peer_id, [PROTOCOL_ID])
+            msg = f"nodo {host.get_id().pretty()} conectado!"
+            msg = 'Se creo un nuevo bloque!'
+            msg = msg.encode()
+
+            await stream.write(msg)
+            await stream.close()
+            response = await stream.read()
+            print(msg.decode())
+            print(response.decode())
+
+
+
+def main() -> None:
+    description = """
+    Conexion simple de nodos usando una red p2p
+    """
+    example_maddr = (
+        "/ip4/127.0.0.1/tcp/8000/p2p/QmQn4SwGkDZKkUEpBRBvTmheQycxAHJUNmVEnjA2v1qe8Q"
+    )
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "-p", 
+        '--port',
+        default=8000, 
+        type=int, 
+        help='Numero de puerto'
+    )
+    parser.add_argument(
+        '-d',
+        "--destination",
+        type=str,
+        help=f'multiaddres de destino, ejemplo {example_maddr}'
+    )
+    args = parser.parse_args()
+    print(args)
+
+    if not args.port:
+        raise RuntimeError('no hay puerto')
+    
+    try:
+        trio.run(run, args.port, args.destination)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
